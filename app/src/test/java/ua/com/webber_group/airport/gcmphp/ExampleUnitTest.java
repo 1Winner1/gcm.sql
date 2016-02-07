d13b37525687c2ff72764a9dc515e3e19eb4dfc8
1Winner1 <htcone810e@gmail.com>
2016-02-07 17:28:16 +0200

    first commit

---
+++ app/src/test/java/ua/com/webber_group/airport/gcmphp/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package ua.com.webber_group.airport.gcmphp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
